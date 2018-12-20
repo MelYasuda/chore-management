c969b5955a90da4a4450c220d97fb6783d7cc75a
Meguru Yasuda <meguruyasuda@Megurus-MacBook-Air.local>
2018-12-20 15:18:21 -0800

    add form

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,11 +1,21 @@
 import React from "react";
 import { Text, View, FlatList } from "react-native";
+import { FormLabel, FormInput, FormValidationMessage, Button } from 'react-native-elements'
 
 export default class Create extends React.Component {
   render(){
     return(
       <View>
-        <Text>This is create screen.</Text>
+        <FormLabel>Chore description</FormLabel>
+        <FormInput/>
+        <FormLabel>Assigned to</FormLabel>
+        <FormInput/>
+        <FormLabel>Priority</FormLabel>
+        <FormInput/>
+        <FormLabel>Notes</FormLabel>
+        <FormInput/>
+        <Button
+        title='ADD' />
       </View>
     );
   }
